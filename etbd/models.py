1ce036d64fb5178c65bf345e9502b2ae0bd47d53
Joy <jtkaufman737@gmail.com>
2018-03-11 17:20:45 -0400

    Migrate models

--- etbd/models.py
+++ etbd/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 
-class Post(models.Model)
+class Post(models.Model):
     author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title=models.CharField(max_length=500)
     text=models.TextField()
@@ -18,4 +18,4 @@ class Post(models.Model)
 
     def __str__(self):
         return self.title
-    )
+    
